3c1e1b3c7555cac556f0d6d16baaeaa5b37c96c8
dev <binteimran853@gmail.com>
2026-01-13 10:47:58 +0500

    Follow pep8 rules on Diamond.py

--- DiamondProblem.py
+++ DiamondProblem.py
@@ -7,14 +7,20 @@ class B(A):
     def show(self):
         print("B.show() Called")
         super().show()
+
+
 class C(A):
     def show(self):
         print("C.show() Called")
         super().show()
+
+
 class D(B,C):
     def show(self):
         print("D.show() Called")
         super().show()
-object=D()
-object.show()
+
+
+obj = D()
+obj.show()
 print(D.mro())
