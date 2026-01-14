7fff3ece7beadb9b75afc75b10b0e364cb5fd695
dev <binteimran853@gmail.com>
2026-01-14 12:25:22 +0500

    Follows pep8 rules

--- Functions.py
+++ Functions.py
@@ -12,16 +12,19 @@
 #         my_function()
 
 def person_details(person):
-    print(f"Name: {person["Name"]}, "
-          f"Role: {person["Role"]}, "
-          f"Email: {person["Email"]}"
+    print(f"Name: {person['Name']}, "
+          f"Role: {person['Role']}, "
+          f"Email: {person['Email']}"
           )
+
 person={
     'Name' : "Aqsa",
     'Role' : "Developer",
     'Email' : "aqsaimran341@gmail.com",
 }
 person_details(person)
+
+# CHECKING THAT POSITIANAL ARGUMENTS MUST COMES FIRST FROM KEYWORD ARGUMENTS
 def my_function(name, role, email):
     return {
         'name' : name,
@@ -30,11 +33,13 @@ def my_function(name, role, email):
     }
 print(my_function('Aqsa', role = 'IT Developer', email = 'aqsaimran341@gmail.com'))
 
+# PRACTICE ON *args
 def arbitrary_test_function(greeting, *names):
     for name in names:
         print(greeting, name )
 
 arbitrary_test_function('Hi,',"Hassan", "Ali", "Arslan", "Javaid")
+
 
 # PRACTICE REAL WORLD EXAMPLE FOR ARBITRARY ARGUMENTS
 def generate_bill(customer_name, service_charge_persent, *items_prices):
@@ -44,12 +49,14 @@ def generate_bill(customer_name, service_charge_persent, *items_prices):
     item_prices = must be valid int or float numbers
     """
     print(f"items_prices: {type(items_prices)}")
-    if not isinstance(customer_name,str) or not customer_name.strip():
+
+    if not isinstance(customer_name, str) or not customer_name.strip():
         raise ValueError("Customer Name must be a valid string.")
     if not isinstance(service_charge_persent, (float, int)) or not service_charge_persent > 0:
         raise ValueError("Service Charge Persent must be a non-negative number")
     if not all ((isinstance(price,(float, int)) and price >= 0) for price in items_prices):
         raise ValueError("Items Prices must be a non-negative number")
+
     sub_total=sum(items_prices)
     service_charge = sub_total * (service_charge_persent / 100)
     total_amount = sub_total + service_charge
@@ -57,8 +64,8 @@ def generate_bill(customer_name, service_charge_persent, *items_prices):
     for i, item in enumerate(items_prices, start = 1):
             print(f"Item {i}: ${item:.2f}")
     return (
-                f"service_charge {service_charge_persent}%: ${service_charge:.2f}\n"
-                f"total_amount: ${total_amount:.2f}"
+            f"service_charge {service_charge_persent}%: ${service_charge:.2f}\n"
+            f"total_amount: ${total_amount:.2f}"
             )
 
 item_prices = []
