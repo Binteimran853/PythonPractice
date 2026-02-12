97024d7915211bd5b6b6edc4087bf6c918701264
dev <binteimran853@gmail.com>
2026-02-12 17:01:47 +0500

    learn about modules

--- ModulePractice.py
+++ ModulePractice.py
@@ -7,3 +7,7 @@ file = sys.argv[0]
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 print(f'File name: {file}', int(sys.argv[1]), int(sys.argv[2]),a+b)
+print('size of object in bytes: ',sys.getsizeof(45))
+print(sys.path)
+print(sys.platform)
+print(sys.version)
